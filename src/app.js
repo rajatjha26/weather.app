6c6112b38ec82a8895126bb9a4aa7b2fbd7df4f8
Rajat jha <rajatjha46@gmail.com>
2020-07-03 00:27:45 +0530

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const forecast=require('./utils/forecast')
 const geocode=require('./utils/geocode')
 
 const app=express()
+const port =process.env.PORT || 3000
 
 //config of handlebars in viewspath, static serve in dirpath and Partials in partialpath 
 const dirpath=path.join(__dirname,'../public/')
@@ -81,6 +82,6 @@ app.get("*",(req,res)=>{
     })
 })
 
-app.listen(3000,()=>{
-    console.log('app is listening on port 3000')
+app.listen(port,()=>{
+    console.log('app is listening on port '+port)
 })
